5656e4c2fcc558b832f9fa885fe2da4b7c0a7b14
sasi <csasikumar98@gmail.com>
2024-07-20 15:37:15 +0530

    added workspace files and other changes to the file tree

--- service.py
+++ service.py
@@ -1,5 +1,7 @@
-from typing import List
+# from typing import List
 
 
-biosamples :List[int] = [222,213,44,37,155] 
-region_ids :List[int] = [1,2,3,4,5]
+biosamples :list[int] = [222,141,213,244] 
+region_ids :list[int] = [1,2,3,4,5]
+sectionNos :list[int] = [1,2,3,4,5]
+
